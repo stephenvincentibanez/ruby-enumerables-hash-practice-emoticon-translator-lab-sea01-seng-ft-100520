6cc43e1a5156001e94aa3c68f1fd2b3b85ac8be3
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 21:08:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,8 +13,9 @@ end
 end
  
 def get_japanese_emoticon(path, emoticon)
-  load_library(path).each do |key, value
-  
+  load_library(path).each do |key, value|
+    binding.pry 
+end 
 end
 
 def get_english_meaning(path, emoticon)
