b04d5c797f2bf33cc14d08a15c47a7b51232a9c5
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 21:01:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,7 @@ def get_english_meaning(path, emoticon)
   load_library(path).each do |key, value|
     if value[:japanese] == emoticon 
       return key
+    else print "Sorry, that emoticon was not found"
     end
     #binding.pry
   end
