e724e9291c0147023a7f04d3c5117a2ab1abce63
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-09-23 20:18:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,22 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = {}
+
+emoticons.each do |feeling, emoticon_set|  
+  emoticon_hash[feeling]={}
+  emoticon_hash[emoticon_set]={}
+  emoticon_hash[feeling][:english]=emoticon_set[0]
+  emoticon_hash[feeling][:japanese]=emoticon_set[1]
+  end
+  #binding.pry
+  emoticon_hash
+  #binding.pry
 end
-
+ 
 def get_japanese_emoticon
   # code goes here
 end
